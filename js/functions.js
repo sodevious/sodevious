20a91072fd77c3b43eca9291384f8ddcef175cd6
Nicole Dominguez <sodevious.net@gmail.com>
2013-01-14 21:43:20 -0500

    Stole some files from another project of mine. Skeletonin'

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+
+/* trigger when page is ready */
+$(document).ready(function() {
+	var d = new Date().getFullYear();
+	$('#copyright_year').text(d);
+});
+
+/* optional triggers
+
+$(window).load(function() {
+	
+});
+
+$(window).resize(function() {
+	
+});
+
+*/
+
